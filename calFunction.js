b317fbee267081292f890f01a49f583372ebe77f
Amanda-G10 <reahana233@gmail.com>
2020-11-30 11:54:52 -1200

    add all

--- calFunction.js
+++ calFunction.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 function grade() {
     var text;
     var C = document.getElementById("myCredit1").value;
@@ -382,3 +383,390 @@ function grade() {
 
     document.getElementById("demo").innerHTML = result;
 }
+=======
+ function grade() {
+var text;
+ var C= document.getElementById("myCredit1").value;
+  var G = document.getElementById("myInput1").value;
+  
+   switch(G) {
+     case "A+":
+      text = 4.00;
+    break;
+   case "A":
+   text =3.75 ;
+    break;
+   case "A-":
+     text =3.50 ;
+    break;
+   case "B+":
+      text =3.25;
+  break;
+     case "B":
+    text = 3;
+     break;
+     case "B-":
+     text =2.75 ;
+    break;
+    case "C+":
+    text =2.50;
+  break;
+     case "C":
+    text = 2.25;
+    break;
+    case "D":
+     text =2.00 ;
+     break;
+    case "F":
+     text =0.00 ;
+     break;
+    default:
+     text = 0;
+    
+   }
+    var s1=text*C;
+ 
+
+
+
+ var C= document.getElementById("myCredit2").value;
+  var G = document.getElementById("myInput2").value;
+  
+   switch(G) {
+     case "A+":
+      text = 4.00;
+    break;
+   case "A":
+   text =3.75 ;
+    break;
+   case "A-":
+     text =3.50 ;
+    break;
+   case "B+":
+      text =3.25;
+  break;
+     case "B":
+    text = 3;
+     break;
+     case "B-":
+     text =2.75 ;
+    break;
+    case "C+":
+    text =2.50;
+  break;
+     case "C":
+    text = 2.25;
+    break;
+    case "D":
+     text =2.00 ;
+     break;
+	 case "F":
+     text =0.00 ;
+     break;
+    default:
+     text = 0;
+    
+   }
+    var s2=text*C;
+ 
+
+
+ var C= document.getElementById("myCredit3").value;
+  var G = document.getElementById("myInput3").value;
+  
+   switch(G) {
+     case "A+":
+      text = 4.00;
+    break;
+   case "A":
+   text =3.75 ;
+    break;
+   case "A-":
+     text =3.50 ;
+    break;
+   case "B+":
+      text =3.25;
+  break;
+     case "B":
+    text = 3;
+     break;
+     case "B-":
+     text =2.75 ;
+    break;
+    case "C+":
+    text =2.50;
+  break;
+     case "C":
+    text = 2.25;
+    break;
+    case "D":
+     text =2.00 ;
+     break;
+   case "F":
+     text =0.00 ;
+     break;
+    default:
+     text =0;
+    
+   }
+     var s3=text*C;
+
+
+
+
+ var C= document.getElementById("myCredit4").value;
+  var G = document.getElementById("myInput4").value;
+  
+   switch(G) {
+     case "A+":
+      text = 4.00;
+    break;
+   case "A":
+   text =3.75 ;
+    break;
+   case "A-":
+     text =3.50 ;
+    break;
+   case "B+":
+      text =3.25;
+  break;
+     case "B":
+    text = 3;
+     break;
+     case "B-":
+     text =2.75 ;
+    break;
+    case "C+":
+    text =2.50;
+  break;
+     case "C":
+    text = 2.25;
+    break;
+    case "D":
+     text =2.00 ;
+     break;
+    case "F":
+     text =0.00 ;
+     break;
+    default:
+     text = 0;
+    
+   }
+     var s4=text*C;
+
+     var C= document.getElementById("myCredit5").value;
+  var G = document.getElementById("myInput5").value;
+  
+   switch(G) {
+     case "A+":
+      text = 4.00;
+    break;
+   case "A":
+   text =3.75 ;
+    break;
+   case "A-":
+     text =3.50 ;
+    break;
+   case "B+":
+      text =3.25;
+  break;
+     case "B":
+    text = 3;
+     break;
+     case "B-":
+     text =2.75 ;
+    break;
+    case "C+":
+    text =2.50;
+  break;
+     case "C":
+    text = 2.25;
+    break;
+    case "D":
+     text =2.00 ;
+     break;
+    case "F":
+     text =0.00 ;
+     break;
+    default:
+     text = 0;
+    
+   }
+    var s5=text*C;
+ 
+
+
+
+ var C= document.getElementById("myCredit6").value;
+  var G = document.getElementById("myInput6").value;
+  
+   switch(G) {
+     case "A+":
+      text = 4.00;
+    break;
+   case "A":
+   text =3.75 ;
+    break;
+   case "A-":
+     text =3.50 ;
+    break;
+   case "B+":
+      text =3.25;
+  break;
+     case "B":
+    text = 3;
+     break;
+     case "B-":
+     text =2.75 ;
+    break;
+    case "C+":
+    text =2.50;
+  break;
+     case "C":
+    text = 2.25;
+    break;
+    case "D":
+     text =2.00 ;
+     break;
+	 case "F":
+     text =0.00 ;
+     break;
+    default:
+     text = 0;
+    
+   }
+    var s6=text*C;
+ 
+
+
+ var C= document.getElementById("myCredit7").value;
+  var G = document.getElementById("myInput7").value;
+  
+   switch(G) {
+     case "A+":
+      text = 4.00;
+    break;
+   case "A":
+   text =3.75 ;
+    break;
+   case "A-":
+     text =3.50 ;
+    break;
+   case "B+":
+      text =3.25;
+  break;
+     case "B":
+    text = 3;
+     break;
+     case "B-":
+     text =2.75 ;
+    break;
+    case "C+":
+    text =2.50;
+  break;
+     case "C":
+    text = 2.25;
+    break;
+    case "D":
+     text =2.00 ;
+     break;
+   case "F":
+     text =0.00 ;
+     break;
+    default:
+     text =0;
+    
+   }
+     var s7=text*C;
+
+
+
+
+ var C= document.getElementById("myCredit8").value;
+  var G = document.getElementById("myInput8").value;
+  
+   switch(G) {
+     case "A+":
+      text = 4.00;
+    break;
+   case "A":
+   text =3.75 ;
+    break;
+   case "A-":
+     text =3.50 ;
+    break;
+   case "B+":
+      text =3.25;
+  break;
+     case "B":
+    text = 3;
+     break;
+     case "B-":
+     text =2.75 ;
+    break;
+    case "C+":
+    text =2.50;
+  break;
+     case "C":
+    text = 2.25;
+    break;
+    case "D":
+     text =2.00 ;
+     break;
+    case "F":
+     text =0.00 ;
+     break;
+    default:
+     text = 0;
+    
+   }
+     var s8=text*C;
+
+     var C= document.getElementById("myCredit9").value;
+  var G = document.getElementById("myInput9").value;
+  
+   switch(G) {
+     case "A+":
+      text = 4.00;
+    break;
+   case "A":
+   text =3.75 ;
+    break;
+   case "A-":
+     text =3.50 ;
+    break;
+   case "B+":
+      text =3.25;
+  break;
+     case "B":
+    text = 3;
+     break;
+     case "B-":
+     text =2.75 ;
+    break;
+    case "C+":
+    text =2.50;
+  break;
+     case "C":
+    text = 2.25;
+    break;
+    case "D":
+     text =2.00 ;
+     break;
+    case "F":
+     text =0.00 ;
+     break;
+    default:
+     text = 0;
+    
+   }
+    var s9=text*C;
+
+	 //CGPA
+   var TR= document.getElementById("totalCredit").value;;
+  var result=(s1+s2+s3+s4+s5+s6+s7+s8+s9)/TR;
+
+  document.getElementById("demo").innerHTML =result; 
+ }
+  
+>>>>>>> ab9167a13b6509d6b83bc646d193875578e9863a
